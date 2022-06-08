14c88e6f590ad267de3f346c38002a0c2d0a4fd6
Wes Taylor <jamsterwes@gmail.com>
2022-06-08 17:26:30 +0200

    (pp rishi/wes) fixed mishandling of newlines in scanner

--- data_population/DataPopulator.java
+++ data_population/DataPopulator.java
@@ -2,23 +2,30 @@ import java.io.*;
 import java.util.Scanner;
 
 public class DataPopulator {
+    private static String readLine(String line) {
+        Scanner sc = new Scanner(line);
+        sc.useDelimiter(",");
+        String displayName = sc.next();
+        Double unitPrice = sc.nextDouble();
+        boolean byWeight = sc.nextInt() == 1;
+        
+        String script = "";
+        script += "INSERT INTO items (display_name, unit_price, by_weight) VALUES (";
+        script += "'" + displayName + "', ";
+        script += unitPrice.toString() + ", ";
+        script += (byWeight ? "true" : "false") + ");\n";
+        return script;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         // Script text
         String script = "";
 
         // Scan items CSV
         Scanner sc = new Scanner(new File(args[0]));
-        sc.useDelimiter(",");
-        sc.nextLine();
+        sc.useDelimiter("\n");
         while (sc.hasNext()) {
-            String displayName = sc.next();
-            Double unitPrice = sc.nextDouble();
-            boolean byWeight = sc.nextInt() == 1;
-            if (sc.hasNext()) sc.nextLine();
-            script += "INSERT INTO items (display_name, unit_price, by_weight) VALUES (";
-            script += "'" + displayName + "', ";
-            script += unitPrice.toString() + ", ";
-            script += (byWeight ? "true" : "false") + ");\n";
+            script += readLine(sc.nextLine());
         }
         sc.close();
 

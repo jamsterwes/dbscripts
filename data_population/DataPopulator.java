c7d3e21b45a98c5d0eca349289050345279b0c01
“rjchandnani” <rishi.j.chandnani@gmail.com>
2022-06-08 16:47:46 +0200

    added the csv scanner

--- data_population/DataPopulator.java
+++ data_population/DataPopulator.java
@@ -2,7 +2,13 @@ import java.io.*;
 import java.util.Scanner;
 
 public class DataPopulator {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(args[1]));
+        sc.useDelimiter(",");   //sets￿the￿delimiter pattern
+        while (sc.hasNext())  //returns a boolean Value
+        {
+            System.out.print(sc.next()); //find￿and￿returns￿the￿next￿complete￿token￿from￿this￿scanner￿￿
+        }
+        sc.close();//closes￿the￿scanner￿￿
     }
 }
